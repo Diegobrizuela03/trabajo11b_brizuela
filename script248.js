7113bf5d3c19d866f22d16654ef3dc9baa0b9d09
Diegobrizuela03 <140198071+Diegobrizuela03@users.noreply.github.com>
2023-09-03 19:15:54 -0300

    Add files via upload

---
+++ script248.js
@@ -0,0 +1,66 @@
+// var topping = prompt("ingresa un topping");
+// var precio = 0;
+// var helado = 5;
+// var preciofinal = 0;
+
+// if(topping=="oreo"){
+// 		precio = 10;
+// }else if(topping == "kitkat"){
+// 	precio = 15;
+// }else if(topping == "kinder"){
+// 	precio = 25;
+// }else{
+// 		alert("No tenemos este topping");
+// }
+       
+// if (topping=="oreo" || 
+// topping=="kitkat" ||
+// topping == "kinder")
+
+// {preciofinal = helado + precio;
+// alert("El helado cuesta "+"$"+preciofinal);}
+
+
+
+
+// var opciones=Number(prompt ("elija su menú: 1-carne 2-pescado 3-verdura"));
+
+// switch(opciones) {
+//     case 1:
+//         alert("Le recomiendo tomar vino tinto");
+//         break;
+//     case 2:
+//         alert ("Le recomiendo tomar vino blanco");
+//         break;
+//     case 3:
+//         alert ("Le recomiendo tomar agua");
+//         break;
+//     default:
+//         alert ("elija 1 carne, 2 pescado o 3 verdura");
+// }
+
+
+
+
+var provincias = ["Córdoba","Jujuy","Salta","Santa Fe","Buenos Aires"];
+
+for(var i=0;i<provincias.length;i++) {
+    console.log ("Las provincias guardadas son:" + provincias [i])
+}
+
+
+
+
+// var numero=0
+
+// while(numero<11) {
+
+// console.log (numero + " " + "es más chico que 11");
+// numero=numero+1;
+
+// }
+// console.log("fin del bucle")
+
+ 
+
+
